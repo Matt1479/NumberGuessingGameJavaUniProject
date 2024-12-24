8495a173119b80a36d2f990e3d294a9f22226b89
Matt1479 <gitmatt1479@gmail.com>
2024-12-24 18:05:53 +0100

    Fix(GameExitState): Save Player's/Program's data conditionally - when it exists

--- src/states/game/GameExitState.java
+++ src/states/game/GameExitState.java
@@ -8,6 +8,7 @@ import states.DataKeys;
 import states.StateNames;
 import states.entity.player.Player;
 import states.entity.program.Program;
+import utility.Util;
 
 public class GameExitState extends BaseState {
     private Player p;
@@ -20,15 +21,19 @@ public class GameExitState extends BaseState {
         this.in = (Scanner) enterParams.get(DataKeys.in);
         this.stateName = (StateNames) enterParams.get(DataKeys.stateName);
 
-        this.p.changeState(StateNames.PlayerSave, new Hashtable<>() {{
-            put(DataKeys.entity, p);
-            put(DataKeys.in, in);
-        }});
+        if (!this.p.data.isEmpty()) {
+            this.p.changeState(StateNames.PlayerSave, new Hashtable<>() {{
+                put(DataKeys.entity, p);
+                put(DataKeys.in, in);
+            }});
+        }
 
-        this.program.changeState(StateNames.ProgramSave, new Hashtable<>() {{
-            put(DataKeys.entity, program);
-            put(DataKeys.in, in);
-        }});
+        if (!this.program.data.isEmpty()) {
+            this.program.changeState(StateNames.ProgramSave, new Hashtable<>() {{
+                put(DataKeys.entity, program);
+                put(DataKeys.in, in);
+            }});
+        }
 
         // Close scanner when done
         this.in.close();
